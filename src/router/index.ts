12f7348dbfb7052e8c5c5c24a452a54049e07fb7
loserxizhou <52983921+loserxizhou@users.noreply.github.com>
2020-11-24 16:53:39 +0800

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -12,9 +12,10 @@ import { basicRoutes } from './routes/';
 // app router
 const router = createRouter({
   history: createWebHashHistory(),
-  routes: basicRoutes as RouteRecordRaw[],
-  strict: true,
+  routes: basicRoutes as RouteRecordRaw[], // 配置的两个基础路由(不需要权限验证的login和主框架根路由)
+  strict: true, // ?  暂时没搞明白
   scrollBehavior: async (to, from, savedPosition) => {
+    // 大概 使用keep-alive标签后部分安卓机返回缓存页位置不精确问题
     await scrollWaiter.wait();
     if (savedPosition) {
       return savedPosition;
